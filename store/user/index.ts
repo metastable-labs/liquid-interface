a8af10106ead97b4a91c4919e6e814276c37781a
meisterjustice <eziefulejustice@gmail.com>
2024-09-15 14:40:21 +0100

    setup redux - install redux dependencies - setup redux store - setup useSystemsFunctions

---
+++ store/user/index.ts
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+export interface UserState {
+  user: User | null;
+  loadingUser: boolean;
+}
+
+const initialState: UserState = {
+  user: null,
+  loadingUser: false,
+};
+
+export const userReducer = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setLoadingUser: (state, action: PayloadAction<boolean>) => {
+      state.loadingUser = action.payload;
+    },
+
+    setUser: (state, action: PayloadAction<User | null>) => {
+      if (action.payload) {
+        state.user = { ...action.payload };
+      } else {
+        state.user = null;
+      }
+    },
+  },
+});
+
+export const { setLoadingUser, setUser } = userReducer.actions;
+
+export default userReducer.reducer;
