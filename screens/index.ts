68ec090b79da1932dd7dc9a3ec07aa6f3b541034
Justice Eziefule <thechiefje@gmail.com>
2024-10-07 19:01:29 +0100

    feat: Implement Tag Screen

--- screens/index.ts
+++ screens/index.ts
@@ -15,6 +15,8 @@ import DebitDeposit from './deposit/debit';
 
 import LiquidityManagement from './liquidity-management';
 
+import Tag from './tag';
+
 export {
   Home,
   TopGainers,
@@ -28,4 +30,5 @@ export {
   LiquidityManagement,
   CryptoDeposit,
   DebitDeposit,
+  Tag,
 };
