84fc9bf164c3d552197b044dce607594bab9a7c2
Renan Mav <renan.mav@hotmail.com>
2024-10-17 13:15:29 -0300

    SCW integration

---
+++ utils/base64.ts
@@ -0,0 +1,37 @@
+import base64 from '@hexagon/base64';
+
+// ! taken from https://github.com/MasterKale/SimpleWebAuthn/blob/e02dce6f2f83d8923f3a549f84e0b7b3d44fa3da/packages/browser/src/helpers/bufferToBase64URLString.ts
+/**
+ * Convert the given array buffer into a Base64URL-encoded string. Ideal for converting various
+ * credential response ArrayBuffers to string for sending back to the server as JSON.
+ *
+ * Helper method to compliment `base64URLStringToBuffer`
+ */
+export function bufferToBase64URLString(buffer: ArrayBuffer): string {
+  const bytes = new Uint8Array(buffer);
+  let str = '';
+
+  for (const charCode of bytes) {
+    str += String.fromCharCode(charCode);
+  }
+
+  const base64String = btoa(str);
+
+  return base64String.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+}
+
+// ! taken from https://github.com/MasterKale/SimpleWebAuthn/blob/e02dce6f2f83d8923f3a549f84e0b7b3d44fa3da/packages/browser/src/helpers/utf8StringToBuffer.ts
+/**
+ * A helper method to convert an arbitrary string sent from the server to an ArrayBuffer the
+ * authenticator will expect.
+ */
+export function utf8StringToBuffer(value: string): ArrayBuffer {
+  return new TextEncoder().encode(value);
+}
+
+/**
+ * Decode a base64url string into its original string
+ */
+export function base64UrlToString(base64UrlString: string): string {
+  return base64.toString(base64UrlString, true);
+}
