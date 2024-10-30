e704b28e1bc4b2d8e48122b1a9220d3a4e980d34
Renan Mav <renan.mav@hotmail.com>
2024-10-30 13:31:03 -0300

    feat: create ThemeProvider.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,6 @@
 import 'react-native-reanimated';
 
 import { useEffect, useState } from 'react';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -13,16 +12,15 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import '@/init/viem';
 import '@/init/api';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
 import { persistor, store } from '@/store';
 import { LQDStackHeader } from '@/components';
 import { PrivyProvider } from '@/components/providers/PrivyProvider';
+import { ThemeProvider } from '@/components/providers/ThemeProvider';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     ClashDisplayBold: require('../assets/fonts/ClashDisplay/ClashDisplay-Bold.otf'),
     ClashDisplayRegular: require('../assets/fonts/ClashDisplay/ClashDisplay-Regular.otf'),
@@ -69,7 +67,7 @@ export default function RootLayout() {
     <GestureHandlerRootView>
       <ReduxProvider store={store}>
         <PersistGate loading={null} persistor={persistor} onBeforeLift={() => setPersisted(true)}>
-          <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <ThemeProvider>
             <PrivyProvider>
               <Stack screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="(onboarding)" />
