72e595f0fe606f75765cc4f0f72970f1d93e1e92
Renan Mav <renan.mav@hotmail.com>
2024-10-30 13:38:26 -0300

    refactor: create useLoadAppFonts hook

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,6 @@
 import 'react-native-reanimated';
 
 import { useEffect, useState } from 'react';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -14,41 +13,13 @@ import { LQDStackHeader } from '@/components';
 import { PrivyProvider } from '@/components/providers/PrivyProvider';
 import { ThemeProvider } from '@/components/providers/ThemeProvider';
 import { ReduxProvider } from '@/components/providers/ReduxProvider';
+import { useLoadAppFonts } from '@/hooks/useLoadAppFonts';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    ClashDisplayBold: require('../assets/fonts/ClashDisplay/ClashDisplay-Bold.otf'),
-    ClashDisplayRegular: require('../assets/fonts/ClashDisplay/ClashDisplay-Regular.otf'),
-    ClashDisplayMedium: require('../assets/fonts/ClashDisplay/ClashDisplay-Medium.otf'),
-    ClashDisplayLight: require('../assets/fonts/ClashDisplay/ClashDisplay-Light.otf'),
-    ClashDisplayExtraLight: require('../assets/fonts/ClashDisplay/ClashDisplay-Extralight.otf'),
-    ClashDisplaySemibold: require('../assets/fonts/ClashDisplay/ClashDisplay-Semibold.otf'),
-    AeonikBold: require('../assets/fonts/Aeonik/Aeonik-Bold.otf'),
-    AeonikRegular: require('../assets/fonts/Aeonik/Aeonik-Regular.otf'),
-    AeonikMedium: require('../assets/fonts/Aeonik/Aeonik-Medium.otf'),
-    AeonikLight: require('../assets/fonts/Aeonik/Aeonik-Light.otf'),
-    QuantaGroteskProBlack: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-Black.otf'),
-    QuantaGroteskProBlackItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-BlackItalic.otf'),
-    QuantaGroteskProBold: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-Bold.otf'),
-    QuantaGroteskProBoldItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-BoldItalic.otf'),
-    QuantaGroteskProExtraBold: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-ExtraBold.otf'),
-    QuantaGroteskProExtraBoldItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-ExtBdIta.otf'),
-    QuantaGroteskProExtraLight: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-ExtraLight.otf'),
-    QuantaGroteskProExtraLightItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-ExtLtIta.otf'),
-    QuantaGroteskProItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-Italic.otf'),
-    QuantaGroteskProLight: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-Light.otf'),
-    QuantaGroteskProLightItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-LightItalic.otf'),
-    QuantaGroteskProMedium: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-Medium.otf'),
-    QuantaGroteskProMediumItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-MediumItalic.otf'),
-    QuantaGroteskProRegular: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-Regular.otf'),
-    QuantaGroteskProSemiBold: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-SemiBold.otf'),
-    QuantaGroteskProSemiBoldItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-SemBdIta.otf'),
-    QuantaGroteskProThin: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-Thin.otf'),
-    QuantaGroteskProThinItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-ThinItalic.otf'),
-  });
+  const [loaded] = useLoadAppFonts();
   const [isPersisted, setPersisted] = useState(false);
 
   useEffect(() => {
---
+++ hooks/useLoadAppFonts.ts
@@ -0,0 +1,34 @@
+import { useFonts } from 'expo-font';
+
+export function useLoadAppFonts() {
+  return useFonts({
+    ClashDisplayBold: require('../assets/fonts/ClashDisplay/ClashDisplay-Bold.otf'),
+    ClashDisplayRegular: require('../assets/fonts/ClashDisplay/ClashDisplay-Regular.otf'),
+    ClashDisplayMedium: require('../assets/fonts/ClashDisplay/ClashDisplay-Medium.otf'),
+    ClashDisplayLight: require('../assets/fonts/ClashDisplay/ClashDisplay-Light.otf'),
+    ClashDisplayExtraLight: require('../assets/fonts/ClashDisplay/ClashDisplay-Extralight.otf'),
+    ClashDisplaySemibold: require('../assets/fonts/ClashDisplay/ClashDisplay-Semibold.otf'),
+    AeonikBold: require('../assets/fonts/Aeonik/Aeonik-Bold.otf'),
+    AeonikRegular: require('../assets/fonts/Aeonik/Aeonik-Regular.otf'),
+    AeonikMedium: require('../assets/fonts/Aeonik/Aeonik-Medium.otf'),
+    AeonikLight: require('../assets/fonts/Aeonik/Aeonik-Light.otf'),
+    QuantaGroteskProBlack: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-Black.otf'),
+    QuantaGroteskProBlackItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-BlackItalic.otf'),
+    QuantaGroteskProBold: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-Bold.otf'),
+    QuantaGroteskProBoldItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-BoldItalic.otf'),
+    QuantaGroteskProExtraBold: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-ExtraBold.otf'),
+    QuantaGroteskProExtraBoldItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-ExtBdIta.otf'),
+    QuantaGroteskProExtraLight: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-ExtraLight.otf'),
+    QuantaGroteskProExtraLightItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-ExtLtIta.otf'),
+    QuantaGroteskProItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-Italic.otf'),
+    QuantaGroteskProLight: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-Light.otf'),
+    QuantaGroteskProLightItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-LightItalic.otf'),
+    QuantaGroteskProMedium: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-Medium.otf'),
+    QuantaGroteskProMediumItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-MediumItalic.otf'),
+    QuantaGroteskProRegular: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-Regular.otf'),
+    QuantaGroteskProSemiBold: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-SemiBold.otf'),
+    QuantaGroteskProSemiBoldItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-SemBdIta.otf'),
+    QuantaGroteskProThin: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-Thin.otf'),
+    QuantaGroteskProThinItalic: require('../assets/fonts/QuantaGroteskPro/QuantaGroteskPro-ThinItalic.otf'),
+  });
+}
