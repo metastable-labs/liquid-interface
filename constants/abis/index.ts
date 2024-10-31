0f82f108e182f6ce12e17a5fd36819d84a96c6c1
Njoku Emmanuel <kalunjoku123@gmail.com>
2024-10-31 20:17:14 +0100

    chore: implement build user operations utils

--- constants/abis/index.ts
+++ constants/abis/index.ts
@@ -2,5 +2,7 @@ import * as LPSugarABI from './LPSugar.json';
 import * as AerodromePoolABI from './AerodromePoolABI.json';
 import * as AerodromeFactoryABI from './AerodromeFactoryABI.json';
 import * as OffchainOracleABI from './OffchainOracle.json';
-
-export { LPSugarABI, AerodromePoolABI, AerodromeFactoryABI, OffchainOracleABI };
+import * as EntryPointABI from './EntryPoint.json';
+import * as SmartWalletAbi from './SmartWallet.json';
+import * as SmartWalletFactoryAbi from './SmartWalletFactory.json';
+export { LPSugarABI, AerodromePoolABI, AerodromeFactoryABI, OffchainOracleABI, SmartWalletAbi, SmartWalletFactoryAbi, EntryPointABI };
