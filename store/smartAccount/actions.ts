17d278b17e30c6a0075582a9cb73ddd998944443
Njoku Emmanuel <kalunjoku123@gmail.com>
2024-11-05 23:34:18 +0300

    chore: sign transaction using current wallet session

--- store/smartAccount/actions.ts
+++ store/smartAccount/actions.ts
@@ -15,10 +15,11 @@ import { RegistrationOptionsNotAvailableError } from './errors';
 import { setLpBalance, setPositions, setTokenBalance, setTokens } from '../account';
 import api from '@/init/api';
 import { useAuth } from '@/providers';
+import { Hex } from 'viem';
 
 export function useSmartAccountActions() {
   const { dispatch, router, smartAccountState } = useSystemFunctions();
-  const { setSession } = useAuth();
+  const { setSession, session } = useAuth();
 
   const updateRegistrationOptions = (options: CreatePassKeyCredentialOptions) => {
     dispatch(setRegistrationOptions(options.data));
@@ -87,6 +88,10 @@ export function useSmartAccountActions() {
     }
   };
 
+  const signTransaction = async (hash: Hex) => {
+    const signature = await session?.sign({ hash });
+    return signature;
+  };
   const logout = async () => {
     try {
       dispatch(setAddress(null));
@@ -108,5 +113,6 @@ export function useSmartAccountActions() {
     getSmartAccount,
     logout,
     login,
+    signTransaction,
   };
 }
--- utils/calls.ts
+++ utils/calls.ts
@@ -3,10 +3,11 @@ import { buildUserOp, getPaymasterData, getUserOpHash } from './wallet';
 import { Call } from './types';
 import { entryPoint06Address } from 'viem/_types/account-abstraction';
 import { paymasterClient, bundlerClient } from '@/init/viem';
-import { useAuth } from '@/providers';
+import { useSmartAccountActions } from '@/store/smartAccount/actions';
 
 // Main function to execute calls
 export async function makeCalls({ calls, account }: { calls: Call[]; account: Address }) {
+  const { signTransaction } = useSmartAccountActions();
   // Build the user operation
   const op = await buildUserOp(account, bundlerClient, {
     calls,
@@ -39,12 +40,9 @@ export async function makeCalls({ calls, account }: { calls: Call[]; account: Ad
     chainId: 8543n,
   });
 
-  // TODO: get smart wallet from reducer and sign the hash
+  const signature = await signTransaction(hash);
 
-  const signature =
-    '0xc66a718123aa330c0d00439ed337bc6721c20298be9fb50bb0e8723b6340a7dc65bc37d891cb278953722d0a93f6daf784dde4e1396e8f57acc64c8d1ea9a602';
-
-  op.signature = signature;
+  op.signature = signature!;
 
   // Send the user operation
   const opHash = await bundlerClient.sendUserOperation({
