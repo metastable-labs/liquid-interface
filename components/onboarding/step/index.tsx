21ff770500220dddeb20d2ada7fc613bc0f0a873
Iyiola Osuagwu <iyiolaosuagwu1@gmail.com>
2024-11-25 06:48:11 +0100

    fixed image and icon alignment on onboarding (#82)
    * fix: image and icon alignment on onboarding
    
    * fix: align icon to center

--- components/onboarding/step/index.tsx
+++ components/onboarding/step/index.tsx
@@ -20,13 +20,11 @@ const LQDOnboardingStep = ({
 
       <Text style={styles.subtitle}>{subtitle}</Text>
 
-      <View style={[styles.image, imageStyle]}>
-        <Image source={image} resizeMode="contain" />
+      <Image source={image} style={[styles.image, imageStyle]} resizeMode="contain" />
 
-        <Image source={firstArc} style={[{ position: 'absolute', zIndex: -1 }, firstArcStyle]} resizeMode="contain" />
+      <Image source={firstArc} style={[{ position: 'absolute', zIndex: -1 }, firstArcStyle]} resizeMode="contain" />
 
-        <Image source={secondArc} style={[{ position: 'absolute', zIndex: -1 }, secondArcStyle]} resizeMode="contain" />
-      </View>
+      <Image source={secondArc} style={[{ position: 'absolute', zIndex: -1 }, secondArcStyle]} resizeMode="contain" />
     </View>
   );
 };
@@ -60,6 +58,6 @@ const styles = StyleSheet.create({
 
   image: {
     position: 'absolute',
-    bottom: '27%',
+    bottom: '25%',
   },
 });
