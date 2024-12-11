0211733dd8f803ac8907d340d00f9099055b0358
Iyiolaosuagwu <iyiolaosuagwu1@gmail.com>
2024-12-11 11:16:27 +0100

    feature: strategy page

---
+++ screens/create-strategy/strategy-detail.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const StrategyDetail = () => {
+  return (
+    <View>
+      <Text>StrategyDetail</Text>
+    </View>
+  );
+};
+
+export default StrategyDetail;
+
+const styles = StyleSheet.create({});
