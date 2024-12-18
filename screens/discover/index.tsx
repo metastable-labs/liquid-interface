04dbc950a27931aad99359794af06dbb80b4aa02
Iyiola Osuagwu <iyiolaosuagwu1@gmail.com>
2024-12-18 13:14:10 +0100

    Discover Page Search And Filter (#108)
    * feature: like comment
    
    * Discover Search and Filter
    
    * feature: search filter on assests tvl protocals and query
    
    * fix: clean up

--- screens/discover/index.tsx
+++ screens/discover/index.tsx
@@ -1,14 +1,29 @@
-import { StyleSheet, View, FlatList } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, FlatList, Text } from 'react-native';
 import { LQDStrategyCard } from '@/components';
-import { useFeeds } from '@/services/feeds/queries';
 import DefaultFooterLoader from '@/components/flatlist/footer-loader';
 import DiscoverFilters from './filters';
 import Loader from '../home/loader';
+import { useSearchFeeds } from '@/services/discover/queries';
 
 const Discover = () => {
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading, isFetching, isError, error, refetch } = useFeeds();
+  const [searchQuery, setSearchQuery] = useState('');
+  const [minTvl, setMinTvl] = useState('');
+  const [maxTvl, setMaxTvl] = useState('');
+  const [cursor, setCursor] = useState('');
+  const [assets, setAssets] = useState<string[]>([]);
+  const [protocols, setProtocols] = useState<string[]>([]);
 
-  const strategies = data?.pages.flatMap((page) => page.data) || [];
+  const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage, isLoading, refetch } = useSearchFeeds(
+    searchQuery,
+    cursor,
+    minTvl,
+    maxTvl,
+    assets,
+    protocols
+  );
+
+  const feeds = data?.pages.flatMap((page) => page.data) || [];
 
   const loadMoreStrategies = () => {
     if (hasNextPage && !isFetchingNextPage) {
@@ -16,25 +31,34 @@ const Discover = () => {
     }
   };
 
-  if (isLoading) {
-    return <Loader />;
-  }
-
   return (
     <View style={styles.container}>
-      <DiscoverFilters />
-
-      <FlatList
-        refreshing={data && isFetching}
-        onRefresh={refetch}
-        data={strategies}
-        showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => <LQDStrategyCard {...item} />}
-        keyExtractor={(_, index) => index.toString()}
-        contentContainerStyle={styles.strategyContainerStyle}
-        onEndReached={loadMoreStrategies}
-        ListFooterComponent={isFetchingNextPage ? <DefaultFooterLoader /> : null}
+      <DiscoverFilters
+        setSearchQuery={setSearchQuery}
+        setMinTvl={setMinTvl}
+        setMaxTvl={setMaxTvl}
+        setCursor={setCursor}
+        setAssets={setAssets}
+        setProtocols={setProtocols}
       />
+
+      {isLoading && <Loader />}
+
+      {feeds.length > 0 && !isLoading && (
+        <FlatList
+          refreshing={isFetching}
+          onRefresh={refetch}
+          data={feeds}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => <LQDStrategyCard {...item} />}
+          keyExtractor={(_, index) => index.toString()}
+          contentContainerStyle={styles.strategyContainerStyle}
+          onEndReached={loadMoreStrategies}
+          ListFooterComponent={isFetchingNextPage ? <DefaultFooterLoader /> : null}
+        />
+      )}
+
+      {!feeds.length && !isLoading && <Text style={styles.noStrategiesText}>No strategies found</Text>}
     </View>
   );
 };
@@ -53,4 +77,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 12,
     paddingTop: 15,
   },
+  noStrategiesText: {
+    fontSize: 16,
+    fontWeight: '500',
+    textAlign: 'center',
+    fontFamily: 'AeonikMedium',
+    marginTop: 50,
+  },
+  topGainerLoaderOne: { height: 45, width: 45, borderRadius: 100 },
 });
